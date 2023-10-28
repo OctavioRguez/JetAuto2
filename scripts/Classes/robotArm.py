fix: :bug: Fixed start and finish robot movement with trayectory. Changed 'y' coordinate calculation in modelPredict

scripts/Classes/robotArm.py
@@ -5,35 +5,39 @@ import numpy as np
 class robotArm:
     def __init__(self, ls:list) -> None:
         # Initialize each joint
-        self.__q = {"q1":0.0, "q2":0.0, "q3":0.0, "q4":0.0}
-
+        self.__q = {"q1":None, "q2":None, "q3":None, "q4":None}
         # Initialize the length of each link
         self.__l = {"l1":ls[0], "l2":ls[1], "l3":ls[2], "l4":ls[3]}
 
     # Private function for solving the inverse kinematics
     def _inverseKinematics(self, x:float, y:float, z:float) -> None:
-        # Calculate multiple variables
-        Pwx = np.sqrt(x**2 + y**2) - self.__l["l4"]
-        Pwy = z - self.__l["l1"]
-        alpha = np.arctan2(Pwy, Pwx)
-        s = np.sqrt(Pwx**2 + Pwy**2)
+        # Check if the arm is not going to hit
+        if x >= 0.0 and z > -0.16 and (-0.1 > y > 0.1 or x > 0.17 or z > 0.05):
+            Pwx = np.sqrt(x**2 + y**2) - self.__l["l4"]
+            Pwy = z - self.__l["l1"]
+            alpha = np.arctan2(Pwy, Pwx)
+            s = np.sqrt(Pwx**2 + Pwy**2)
 
-        D = (s**2 - self.__l["l2"]**2 - self.__l["l3"]**2)/(2*self.__l["l2"]*self.__l["l3"])
-        # Saturate D for keeping the arm in the workspace
-        D = 0.99 if abs(D) >= 1 else D
+            D = (s**2 - self.__l["l2"]**2 - self.__l["l3"]**2)/(2*self.__l["l2"]*self.__l["l3"])
+            # Check if the point is reachable
+            if abs(D) < 1.0:
+                # Calculate the joint angles
+                self.__q["q1"] = np.arctan2(y, x)
+                self.__q["q3"] = -np.arctan2(np.sqrt(1 - D**2), D)
 
-        # Calculate the joint angles
-        self.__q["q1"] = np.arctan2(y, x)
-        self.__q["q3"] = -np.arctan2(np.sqrt(1 - D**2), D)
-
-        gamma = np.arctan2(self.__l["l3"]*np.sin(self.__q["q3"]), self.__l["l2"] + self.__l["l3"]*np.cos(self.__q["q3"]))
-        self.__q["q2"] = alpha - gamma
-        self.__q["q4"] = -(self.__q["q2"] + self.__q["q3"])
+                gamma = np.arctan2(self.__l["l3"]*np.sin(self.__q["q3"]), self.__l["l2"] + self.__l["l3"]*np.cos(self.__q["q3"]))
+                self.__q["q2"] = alpha - gamma
+                self.__q["q4"] = -(self.__q["q2"] + self.__q["q3"])
+                return
+        for qn in self.__q.keys():
+            self.__q[qn] = None
 
     # Private function for getting the final trajectory
-    def _trajectory(self, qIni:list, qFin:list, rate:int) -> list:       
+    def _trajectory(self, qIni:list, qFin:list, rate:int) -> list:
+        if any(q is None for q in self.__q.values()):
+            return np.array([])
         return np.linspace(qIni, qFin, rate)
-    
+
     # Private function for starting the joints
     def _startArm(self) -> list:
         self.__q["q1"] = 0.0
@@ -52,6 +56,8 @@ class robotArm:
 
     # Public function for getting the joints
     def getJoints(self) -> list:
+        if any(q is None for q in self.__q.values()):
+            return None
         self.__q["q2"] = np.pi/2 - self.__q["q2"] # Add pi/2 offset for the joint 2 angle
         self.__q["q3"] *= (-1) # Invert the joint 3 angle
         self.__q["q4"] *= (-1) # Invert the joint 4 angle

scripts/armTrajectory.py
@@ -57,13 +57,13 @@ class armTrajectory:
     def _start(self) -> None:
         print("The Arm Movement node is Running")
         joints = self.__jointsManager._startArm()
-        self.jointsPublish(joints)
+        self.__trajectory = np.flip(self.__jointsManager._trajectory(self.__jointsPosition, joints, self.__rate))
 
     # Reset the arm position when the node is shutdown
     def _stop(self) -> None:
         print("Stopping the Arm Movement node")
         joints = self.__jointsManager._resetArm()
-        self.jointsPublish(joints)
+        self.__trajectory = np.flip(self.__jointsManager._trajectory(self.__jointsPosition, joints, self.__rate))
 
 if __name__ == '__main__':
     # Initialize ROS node